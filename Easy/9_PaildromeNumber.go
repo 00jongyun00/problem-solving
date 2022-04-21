4e24bc22f9c1d36ecfb4ae71caa0d442206dabeb
nothingprogram <dev@jongyun.com>
2022-04-21 22:17:46 +0900

    Eash 9 is palidrome number

---
+++ Easy/9_PaildromeNumber.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func isPalindrome(x int) bool {
+	sum := 0
+	compare := x
+
+	for x > 0 {
+		r := x % 10
+		sum = (sum * 10) + r
+		x /= 10
+	}
+	return sum == compare
+}
+
+func main() {
+	x := 121
+	fmt.Println(isPalindrome(x))
+}
