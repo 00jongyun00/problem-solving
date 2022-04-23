12fb0c6d0149e674771ce9a614ed9a3e5276f41c
nothingprogram <dev@jongyun.com>
2022-04-23 21:17:14 +0900

    Easy 20 valid parenthese

---
+++ Easy/20_ValidParentheses.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+func isValid(s string) bool {
+	if len(s)%2 != 0 {
+		return false
+	}
+
+	var stack []rune
+	open := map[rune]rune{
+		'(': ')',
+		'[': ']',
+		'{': '}',
+	}
+
+	for _, r := range s {
+		if closer, ok := open[r]; ok {
+			stack = append(stack, closer)
+			continue
+		}
+
+		l := len(stack) - 1
+		if l < 0 || r != stack[l] {
+			return false
+		}
+		stack = stack[:l]
+	}
+	return len(stack) == 0
+}
+
+func main() {
+	fmt.Println(isValid("()[]{}"))
+}
