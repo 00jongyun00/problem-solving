feecda31231243d4e6b06ef37690d0eb49e47563
jongyunha <gkwhddbs9999@gmail.com>
2022-05-15 15:55:33 +0900

    Programmers level2 압축 문자열 재도전

--- programmers/src/main/java/level2/CompressionOfStrings.java
+++ programmers/src/main/java/level2/CompressionOfStrings.java
@@ -1,28 +1,31 @@
 package level2;
 
+import static java.lang.Math.min;
+
 public class CompressionOfStrings {
 
     public static int Solution(String s) {
         int answer = s.length();
 
-        for (int i = 1; i <= s.length() / 2; i++) {
+        for (int i = 1; i <= s.length() / 2 + 1; i++) {
             int zipLevel = 1;
             String zipStr = s.substring(0, i);
-            StringBuilder result = new StringBuilder();
+            StringBuilder sb = new StringBuilder();
 
             for (int j = i; j <= s.length(); j += i) {
-                String next = s.substring(j, Math.min(j + i, s.length()));
+                String next = s.substring(j, min(j + i, s.length()));
                 if (zipStr.equals(next)) {
                     zipLevel++;
                 } else {
-                    result.append(zipLevel != 1 ? zipLevel : "").append(zipStr);
+                    sb.append(zipLevel != 1 ? zipLevel : "").append(zipStr);
                     zipStr = next;
                     zipLevel = 1;
                 }
             }
-            result.append(zipStr);
-            answer = Math.min(answer, result.length());
+            sb.append(zipStr);
+            answer = min(sb.length(), answer);
         }
+
         return answer;
     }
 
