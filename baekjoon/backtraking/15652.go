7b5425246ca3d2c62632ae5729f4fe596583645f
jongyunha <gkwhddbs9999@gmail.com>
2022-05-22 20:19:54 +0900

    N과 M 중복 허용

--- baekjoon/backtraking/15652.go
+++ baekjoon/backtraking/15652.go
@@ -1,1 +1,39 @@
 package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+var N, M int
+var reader *bufio.Reader
+var writer *bufio.Writer
+var result, check []int
+
+func DFS(at, depth int) {
+	if depth == M {
+		for _, r := range result {
+			fmt.Fprintf(writer, "%d ", r)
+		}
+		fmt.Fprintln(writer)
+		return
+	}
+	for i := at; i <= N; i++ {
+		result[depth] = i
+		DFS(i, depth+1)
+	}
+}
+
+func main() {
+
+	reader = bufio.NewReader(os.Stdin)
+	writer = bufio.NewWriter(os.Stdout)
+
+	defer writer.Flush()
+
+	fmt.Fscanf(reader, "%d %d\n", &N, &M)
+	result = make([]int, M)
+	check = make([]int, N+1)
+	DFS(1, 0)
+}
