ff827d439d863f18837b5f7baf9620819d617f10
jongyunha <gkwhddbs9999@gmail.com>
2022-05-22 20:19:39 +0900

    N-queen 주석 추가

--- baekjoon/backtraking/9663.go
+++ baekjoon/backtraking/9663.go
@@ -11,6 +11,7 @@ var N, result int
 var board []int
 
 func DFS(depth int) {
+	// 모든 행에 Queen 이 놓였을때 result 를 +1 하고 return
 	if depth == N {
 		result++
 		return
@@ -19,6 +20,7 @@ func DFS(depth int) {
 	for i := 0; i < N; i++ {
 		board[depth] = i
 
+		// depth (행) i (열) 의 현재 행렬 좌표에 Queen 을 놓을 수 있는지 확인 한 다음 가능 하면 재귀호출
 		if possible(depth) {
 			DFS(depth + 1)
 		}
@@ -27,10 +29,12 @@ func DFS(depth int) {
 
 func possible(col int) bool {
 	for i := 0; i < col; i++ {
+		// 현재 재귀 호출된 depth (행) 의 열 과 depth 전 까지의 i (행) 의 열 이 같은 선상에 있으면 false
 		if board[col] == board[i] {
 			return false
 		}
 
+		// 행 끼리 의 차 와 열 끼리 의 차가 동일 하면 false
 		if math.Abs(float64(col-i)) == math.Abs(float64(board[col]-board[i])) {
 			return false
 		}
